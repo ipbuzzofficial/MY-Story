41ced4458d311d5a983fed7f8023a744f3645d99
Arun <ak1577282@gmail.com>
2024-02-23 22:00:03 +0530

    create pages with react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import Siggnup from './pages/Siggnup'
 
 
 
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-     <h1>Hello</h1>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/sign-in' element={<Signin/>} />
+      <Route path='/sign-up' element={<Siggnup/>} />
+      <Route path='/dashboard' element={<Dashboard/>} />
+      <Route path='/projects' element={<Projects/>} />
+      
+
+    </Routes>
+    
+    
+    </BrowserRouter>
   )
 }
-
-export default App
